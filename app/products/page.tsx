2fa698c5b3b452ef01d0ab866867c5f680a1783c
AkshitMehta2005 <akshitmehta494@gmail.com>
2026-03-17 13:54:23 +0530

    footer

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function ProductsPage() {
-  redirect("https://innosent.websoo.site/shops/");
+  redirect("https://shop.innosent.co.in/");
 }
